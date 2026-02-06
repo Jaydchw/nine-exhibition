a3b66c4e18eebc6e1b909db024d51c021f453003
Jayden Holdsworth <70723772+Jaydchw@users.noreply.github.com>
2026-02-06 18:52:14 +0000

    chore: add gabbys details

--- src/lib/artists.ts
+++ src/lib/artists.ts
@@ -20,7 +20,7 @@ export const artists = [
       "Ellie Seal is a twenty one year old local artist. Her practice mainly consists of oil paint, sculpting, and dance. As influence to her own work, Seal finds inspiration from artists such as Hilma Af klint and Wassily Kandinsky. Seal's project addresses an expression of her insecurities and causes through dance. With abstract shapes and colours Seal will create pieces with movement and powerful expression.",
     instagram: "https://www.instagram.com/small_wheels20/",
   },
-  { name: "Freya Pearson", age: "20", description: "", instagram: "" },
+  { name: "Freja Pearson", age: "20", description: "", instagram: "" },
   { name: "Grace Chapman", age: "20", description: "", instagram: "" },
   { name: "Megan Latham", age: "30 something", description: "", instagram: "" },
   { name: "Lou Ives", age: "TBA", description: "", instagram: "" },
